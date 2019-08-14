ec6dc783a75c721ba1dd4396dd0b574910bec828
Emma Davenport <davenport.physics@gmail.com>
2019-08-13 20:09:02 -0500

    DrawObject and DrawType added

--- src/drawlayers.c
+++ src/drawlayers.c
@@ -1,3 +1,42 @@
+
+typedef enum DrawType 
+{
+
+    MENU = 0,
+    BUTTON,
+    POPUP
+
+} DrawType;
+
+
+typedef struct DrawObject 
+{
+
+    DrawType type;
+    float x_pos;
+    float y_pos;
+    union {
+
+        struct {
+
+            char picture_path[256];
+
+        } menu;
+
+    } object;
+
+} DrawObject;
+
+typedef struct DrawLayer 
+{
+
+
+
+} DrawLayer;
+
+static DrawLayer draw_layers[10];
+static unsigned int current_draw_layer = 0;
+
 void HandleMouseClickInButtonAreas(int x, int y) 
 {
 
