3563c0e5f9bd40b54cdfe01013c1d33ca6db1535
Emma Davenport <davenport.physics@gmail.com>
2019-08-15 19:22:59 -0500

    Check area of mouse click before calling callback

--- src/drawlayers.c
+++ src/drawlayers.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 
 typedef enum DrawType 
@@ -17,8 +18,8 @@ typedef struct DrawObject
 {
 
     DrawType type;
-    float x_pos;
-    float y_pos;
+    float x;
+    float y;
     float width;
     float height;
     union {
@@ -50,6 +51,10 @@ typedef struct DrawLayer
 static DrawLayer *draw_layers;
 static unsigned int current_draw_layer = 0;
 
+
+bool HandleMouseClick(DrawObject *object, int x, int y);
+bool IsMouseClickInAreaOfObject(DrawObject *object, int x, int y);
+
 void InitializeDrawLayers() 
 {
 
@@ -62,15 +67,36 @@ void HandleMouseClickInButtonAreas(int x, int y)
 
     for (int i = 0; i < draw_layers[current_draw_layer].num_objects;i++) {
 
+        if (draw_layers[current_draw_layer].objects[i].type == BUTTON) {
 
-        DrawObject *object = &draw_layers[current_draw_layer].objects[i];
-        if (object->type == BUTTON) {
-
-            object->member.button.Callback();
+            HandleMouseClick(&draw_layers[current_draw_layer].objects[i], x, y);
 
         }
 
     }
+
+}
+
+bool HandleMouseClick(DrawObject *object, int x, int y) 
+{
+
+    if (IsMouseClickInAreaOfObject(object, x, y)) {
+
+        object->member.button.Callback();
+
+    }
+
+}
+
+bool IsMouseClickInAreaOfObject(DrawObject *object, int x, int y) 
+{
+
+    if (object->x > x || x > object->x + object->width)
+        return false;
+    if (object->y > y || y > object->y + object->height)
+        return false;
+
+    return true;
 
 }
 
