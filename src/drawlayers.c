c28c404ecb8e5f911b80c90e0799fe658b41d852
Emma Davenport <davenport.physics@gmail.com>
2019-08-15 19:06:30 -0500

    Added button callback loop

--- src/drawlayers.c
+++ src/drawlayers.c
@@ -1,4 +1,8 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+
 typedef enum DrawType 
 {
 
@@ -15,6 +19,8 @@ typedef struct DrawObject
     DrawType type;
     float x_pos;
     float y_pos;
+    float width;
+    float height;
     union {
 
         struct {
@@ -23,22 +29,62 @@ typedef struct DrawObject
 
         } menu;
 
-    } object;
+        struct {
+
+            void (*Callback)();
+
+        } button;
+
+    } member;
 
 } DrawObject;
 
 typedef struct DrawLayer 
 {
 
-
+    unsigned int num_objects;
+    DrawObject objects[256];
 
 } DrawLayer;
 
-static DrawLayer draw_layers[10];
+static DrawLayer *draw_layers;
 static unsigned int current_draw_layer = 0;
+
+void InitializeDrawLayers() 
+{
+
+    draw_layers = malloc(sizeof(DrawLayer) * 10);
+
+}
 
 void HandleMouseClickInButtonAreas(int x, int y) 
 {
 
-    
+    for (int i = 0; i < draw_layers[current_draw_layer].num_objects;i++) {
+
+
+        DrawObject *object = &draw_layers[current_draw_layer].objects[i];
+        if (object->type == BUTTON) {
+
+            object->member.button.Callback();
+
+        }
+
+    }
+
+}
+
+void CleanUpLayers() 
+{
+
+    free(draw_layers);
+
+}
+
+void ClearLayers() 
+{
+
+    CleanUpLayers();
+    InitializeDrawLayers();
+
 }
--- src/drawlayers.h
+++ src/drawlayers.h
@@ -1,6 +1,7 @@
 #ifndef DRAWLAYERS_H
 #define DRAWLAYERS_H
 
+void InitializeDrawLayers();
 void HandleMouseClickInButtonAreas(int x, int y);
 
 #endif
