fc372f763698fd2fca24c2f9506c341a7c6ae67b
Emma Davenport <davenport.physics@gmail.com>
2019-08-31 00:10:53 -0500

    Fixed issue where a pointer was only being initalized within the scope of a single function

--- src/jsonlayer.c
+++ src/jsonlayer.c
@@ -8,11 +8,12 @@
 #include "shared.h"
 #include "jsonlayer.h"
 
-void SetJsonObjectFromFile(json_object *object, const char *file);
+void SetJsonObjectFromFile(json_object **object, const char *file);
 void ParseCompanyJsonObject();
 void ParseJsonDrawObject();
 
 array_list *GetJsonObjectArray(json_object *object, const char *json_path);
+const char* GetStringFromJsonObject(json_object *object, const char *json_path);
 
 json_object *companies    = NULL;
 json_object *draw_objects = NULL;
@@ -20,26 +21,27 @@ json_object *draw_objects = NULL;
 void *JsonEntry(ALLEGRO_THREAD *thread, void *arg) 
 {
 
-    SetJsonObjectFromFile(companies   , "assets/config/companies.json");
-    SetJsonObjectFromFile(draw_objects, "assets/config/drawobjects.json");
+    SetJsonObjectFromFile(&companies   , "assets/config/companies.json");
+    SetJsonObjectFromFile(&draw_objects, "assets/config/drawobjects.json");
+    GetStringFromJsonObject(companies, "/Test");
     ParseCompanyJsonObject();
     ParseJsonDrawObject();
 
 }
 
-void SetJsonObjectFromFile(json_object *object, const char *file) 
+void SetJsonObjectFromFile(json_object **object, const char *file) 
 {
 
-    object = json_object_from_file(file);
-    if (object == -1) {
+    *object = json_object_from_file(file);
+    if (*object == NULL) {
 
         LogF("Error %s", json_util_get_last_err());
         SetCleanUpToTrue();
 
     } else {
 
-        LogF("From %s retrieved %s", file, json_object_to_json_string(object));
-        
+        LogF(json_object_to_json_string(*object));
+
     }
 
 }
@@ -47,23 +49,23 @@ void SetJsonObjectFromFile(json_object *object, const char *file)
 void ParseCompanyJsonObject() 
 {
 
-    if (companies == -1)
+    if (companies == NULL)
         return;
 
     array_list *companies_list = GetJsonObjectArray(companies, "/Companies");
 
-    if (companies_list == NULL) {
-
+    if (companies_list == NULL) 
         SetCleanUpToTrue();
+    else
+        LogF("Got companies array of length %d", companies_list->length);
 
-    }
 
 }
 
 void ParseJsonDrawObject() 
 {
 
-    if (draw_objects == -1)
+    if (draw_objects == NULL)
         return;
 
 }
@@ -72,9 +74,8 @@ array_list *GetJsonObjectArray(json_object *object, const char *json_path)
 {
 
     json_object *store_object = NULL;
-    int code                  = json_pointer_get(object, json_path, &store_object);
 
-    if (code != 0)
+    if (json_pointer_get(object, json_path, &store_object) != 0)
         LogF("Unable to find array at path %s", json_path);
     else
         return json_object_get_array(store_object);
@@ -82,3 +83,22 @@ array_list *GetJsonObjectArray(json_object *object, const char *json_path)
     return NULL;
 
 }
+
+const char* GetStringFromJsonObject(json_object *object, const char *json_path) 
+{
+
+    json_object *store_object = NULL;
+    
+    if (object == NULL)
+        Log("Object is null");
+
+    int code = json_pointer_get(object, json_path, &store_object);
+    if (code != 0)
+        LogF("Unable to find %s. Code = %d", json_path, code);
+    else
+        return json_object_get_string(store_object);
+
+    return "";
+
+}
+
