e625694bde05c539a41204de5732202b96f2d249
Emma Davenport <edavenport@localhost.localdomain>
2019-09-08 00:03:00 -0500

    Use rpath while compiling under linux. Copy dynamic libs for linux in build script

--- tools/build.sh
+++ tools/build.sh
@@ -1,55 +1,68 @@
 #!/bin/sh
 
 declare build_dir=""
+declare operating_system="$(expr substr $(uname -s) 1 10)"
 
+echo "Detected operating system = ${operating_system}"
 DebugBuild() 
 {
 
-    cmake .. -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_INSTALL_PREFIX="/mingw64/bin" -DCMAKE_BUILD_TYPE=Debug
+
     build_dir="build-debug-$(date +'%m-%d-%Y')"
     mkdir $build_dir
+
+    if [ $operating_system == "MINGW64_NT" ]; then
+
+        cmake .. -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_INSTALL_PREFIX="/mingw64/bin" -DCMAKE_BUILD_TYPE=Debug
+
+    elif [ $operating_system == "Linux" ]; then
+
+        echo "cmake"
+        cmake .. -DCMAKE_BUILD_TYPE=Debug
+
+    fi
 
 }
 
 ReleaseBuild() 
 {
 
-    cmake .. -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_INSTALL_PREFIX="/mingw64/bin" -DCMAKE_BUILD_TYPE=Release
     build_dir="build-release-$(date +'%m-%d-%Y')"
     mkdir $build_dir
 
-}
+    if [ $operating_system == "MINGW64_NT" ]; then
 
-WindowsLibs() 
-{
+        cmake .. -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_INSTALL_PREFIX="/mingw64/bin" -DCMAKE_BUILD_TYPE=Release
 
-    declare msys_path="C:/msys64/mingw64/bin/"
-    for dll in "allegro_acodec-5.2.dll" "allegro_audio-5.2.dll" "allegro_font-5.2.dll" "allegro_image-5.2.dll" "allegro_ttf-5.2.dll" "allegro_video-5.2.dll" "allegro-5.2.dll" "libdumb.dll" \
-               "libFLAC-8.dll" "libjson-c-4.dll" "libogg-0.dll" "libopenal-1.dll" "libopus-0.dll" "libopusfile-0.dll" "libsqlite3-0.dll" "libtheoradec-1.dll" "libvorbis-0.dll" "libvorbisfile-3.dll" ; do
+    elif [ $operating_system == "Linux" ]; then
 
-        cp "${msys_path}${dll}" $build_dir
+        cmake .. -DCMAKE_BUILD_TYPE=Release
 
-    done
-
-}
-
-LinuxLibs() 
-{
-
-    echo "STUB"
+    fi
 
 }
 
 GetSharedLibs() 
 {
 
-    if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    if [ $operating_system == "MINGW64_NT" ]; then
 
-        WindowsLibs
+        declare msys_path="C:/msys64/mingw64/bin/"
+        for dll in "allegro_acodec-5.2.dll" "allegro_audio-5.2.dll" "allegro_font-5.2.dll" "allegro_image-5.2.dll" "allegro_ttf-5.2.dll" "allegro_video-5.2.dll" "allegro-5.2.dll" "libdumb.dll" \
+                   "libFLAC-8.dll" "libjson-c-4.dll" "libogg-0.dll" "libopenal-1.dll" "libopus-0.dll" "libopusfile-0.dll" "libsqlite3-0.dll" "libtheoradec-1.dll" "libvorbis-0.dll" "libvorbisfile-3.dll" ; do
 
-    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+            cp "${msys_path}${dll}" $build_dir
 
-        LinuxLibs
+        done
+
+    elif [ $operating_system == "Linux" ]; then
+
+        for lib in $(readelf -d projectstocks | grep 'Shared library' | awk '{print $5}' | tr -d []); do
+
+            #cp -L $(locate $lib | head -n 1) "lib/${lib}"
+            cp -L "/lib64/${lib}" "lib/${lib}"
+
+        done
 
     fi
 
@@ -63,7 +76,7 @@ if [ $1 == "--debug" ]; then
 
 elif [ $1 == "--release" ]; then
 
-    echo "Building debug release"
+    echo "Building release version"
     ReleaseBuild
 
 else
@@ -73,8 +86,27 @@ else
 
 fi
 
+if [ $operating_system == "MINGW64_NT" ]; then
 
-mingw32-make -j2
+    mingw32-make -j2
+
+elif [ $operating_system == "Linux" ]; then
+
+    make -j2
+
+fi
+
 GetSharedLibs
-cp projectstocks.exe $build_dir
+
+if [ $operating_system == "MINGW64_NT" ]; then
+
+    cp projectstocks.exe $build_dir
+
+elif [ $operating_system == "Linux" ]; then
+
+    cp projectstocks $build_dir
+    cp -r lib $build_dir
+
+fi
+
 cp -r assets $build_dir
