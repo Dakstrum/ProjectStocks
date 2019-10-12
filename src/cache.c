89e40b565a374aba4aaa24ac929934f0538e4595
Emma Davenport <davenport.physics@gmail.com>
2019-10-12 13:31:48 -0500

    Renamed cache to bitmap_cache. Added font cache struct

--- src/cache.c
+++ src/cache.c
@@ -1,27 +1,38 @@
 #include <stdlib.h>
 
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
 
 #include "log.h"
 
-typedef struct Cache {
+typedef struct BitmapCache {
 
     char *asset_path;
     ALLEGRO_BITMAP *bitmap;
 
-} Cache;
+} BitmapCache;
+
+typedef struct FontCache {
+
+    char *asset_path;
+    ALLEGRO_FONT *font;
+    int font_size;
+
+
+} FontCache;
 
 ALLEGRO_BITMAP *GetNewlyAddedBitmapFromCache(char *asset_path);
 
-static Cache *bitmap_cache;
-static unsigned int cache_size  = 256;
-static unsigned int cache_index = 0;
+static BitmapCache *bitmap_cache;
+static FontCache   *font_cache;
+static unsigned int bitmap_cache_size  = 256;
+static unsigned int bitmap_cache_index = 0;
 
 void InitializeCache() 
 {
 
-    bitmap_cache = malloc(sizeof(Cache) * cache_size);
-    for (int i = 0; i < cache_size; i++)
+    bitmap_cache = malloc(sizeof(BitmapCache) * bitmap_cache_size);
+    for (int i = 0; i < bitmap_cache_size; i++)
         bitmap_cache[i].asset_path = NULL;
 }
 
@@ -31,7 +42,7 @@ ALLEGRO_BITMAP *GetBitmapFromCache(char *asset_path)
     if (asset_path == NULL || strcmp(asset_path,"") == 0)
         return NULL;
 
-    for (int i = 0; i < cache_index; i++) {
+    for (int i = 0; i < bitmap_cache_index; i++) {
 
         if (strcmp(asset_path, bitmap_cache[i].asset_path) == 0)
             return bitmap_cache[i].bitmap;
@@ -45,11 +56,11 @@ ALLEGRO_BITMAP *GetBitmapFromCache(char *asset_path)
 void IncreaseCacheSizeIfNeeded() 
 {
 
-    if (cache_index + 1 < cache_size)
+    if (bitmap_cache_index + 1 < bitmap_cache_size)
         return;
 
-    cache_size += 256;
-    bitmap_cache = realloc(bitmap_cache, sizeof(Cache *) * cache_size);
+    bitmap_cache_size += 256;
+    bitmap_cache = realloc(bitmap_cache, sizeof(BitmapCache *) * bitmap_cache_size);
     Log("Increase bitmap_cache");
 
 }
@@ -58,10 +69,10 @@ ALLEGRO_BITMAP *GetNewlyAddedBitmapFromCache(char *asset_path)
 {
 
     IncreaseCacheSizeIfNeeded();
-    bitmap_cache[cache_index].asset_path = asset_path;
-    bitmap_cache[cache_index].bitmap     = al_load_bitmap(asset_path);
-    cache_index++;
+    bitmap_cache[bitmap_cache_index].asset_path = asset_path;
+    bitmap_cache[bitmap_cache_index].bitmap     = al_load_bitmap(asset_path);
+    bitmap_cache_index++;
 
-    return bitmap_cache[cache_index-1].bitmap;
+    return bitmap_cache[bitmap_cache_index-1].bitmap;
     
 }
