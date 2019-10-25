58838b0e6911ff180efea163cb5105f191f21e56
Jack Soderstrom <JaxSoder@gmail.com>
2019-10-24 19:53:55 -0500

    Added buy and sell buttons

--- src/stocksmenu.c
+++ src/stocksmenu.c
@@ -11,7 +11,9 @@
 #include "cache.h"
 
 
-static MenuWithChilds *stocks_menu = NULL;
+static MenuWithChilds *stocks_menu           = NULL;
+static MenuWithChilds *sell_transaction_menu = NULL;
+static MenuWithChilds *buy_transaction_menu  = NULL;
 
 void DisplayTempPopUp();
 
@@ -57,6 +59,42 @@ void RenderStocksMenu()
 {
 
     DrawLayers();
+
+}
+
+void StocksSellButtonCallBack()
+{
+
+    if (sell_transaction_menu == NULL) {
+
+        CreateNewDrawLayer();
+        sell_transaction_menu = GetMenuWithChildsFromJsonLayer("SellTransactionMenu");
+        AddMenuWithChildsToDrawLayer(sell_transaction_menu);
+        
+    } else {
+
+        ClearCurrentDrawLayer();
+        sell_transaction_menu = NULL;
+
+    }
+
+}
+
+void StocksBuyButtonCallBack()
+{
+
+    if (buy_transaction_menu == NULL) {
+
+        CreateNewDrawLayer();
+        buy_transaction_menu = GetMenuWithChildsFromJsonLayer("BuyTransactionMenu");
+        AddMenuWithChildsToDrawLayer(buy_transaction_menu);
+        
+    } else {
+
+        ClearCurrentDrawLayer();
+        buy_transaction_menu = NULL;
+
+    }
 
 }
 
