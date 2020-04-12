0091b64d641cda71407dcb7b570bc72b24462387
Emma Davenport <davenport.physics@gmail.com>
2020-04-11 21:14:22 -0500

    Partial Implementation of retrieving all saves

--- include/db/dbsave.h
+++ include/db/dbsave.h
@@ -1,11 +1,28 @@
 #ifndef DBSAVE_H
 #define DBSAVE_H
 
+typedef struct PlayerSaves {
+
+    int *save_id;
+    int *save_player_id;
+    char **save_name;
+    char **save_player_name;
+    double *save_player_money;
+    unsigned int *time_spent_in_game;
+    unsigned int *game_seed;
+
+    int num_saves;
+    int size;
+
+} PlayerSaves;
+
 int InsertSave(char *save_name, char *player_name, unsigned int game_seed);
 unsigned int GetSaveSeedWithSaveId(int save_id);
 void DeleteAccountSave(char *save_name, char *player_name);
 
 int InsertSaveEntry(char *save_name, unsigned int game_seed);
 int InsertPlayerEntry(int save_id, char *player_name, double money, int save_owner);
+
+PlayerSaves *GetAllSaves();
 
 #endif
--- src/db/dbsave.c
+++ src/db/dbsave.c
@@ -4,6 +4,7 @@
 #include <sqlite3.h>
 
 #include "log.h"
+#include "dbsave.h"
 #include "dbaccess.h"
 #include "account.h"
 
@@ -43,29 +44,6 @@ int InsertSaveEntry(char *save_name, unsigned int game_seed)
     }
     sqlite3_close(db);
     return save_id;
-}
-
-int InsertSave(char *save_name, char *player_name, unsigned int game_seed)
-{
-
-    int save_id = InsertSaveEntry(save_name, game_seed);
-    if (save_id == -1) {
-
-        Log("Unable to create save");
-        return -1;
-
-    }
-
-    int player_id = InsertPlayerEntry(save_id, player_name, 3000.0, 1);
-    if (player_id == -1) {
-
-        Log("Unable to create player");
-        return -1;
-
-    }
-
-    return save_id;
-
 }
 
 void DeleteAccountSave(char *save_name, char *player_name)
@@ -162,5 +140,71 @@ char *GetPlayerNameFromSaveName(char *save_name)
     sqlite3_close(db);
 
     return player_name;
+
+}
+
+
+void ResizePlayerSavesElements(PlayerSaves *saves)
+{
+
+    saves->size += 16;
+    saves->save_id            = realloc(saves->save_id, sizeof(int) * saves->size);
+    saves->save_player_id     = realloc(saves->save_player_id, sizeof(int) * saves->size);
+    saves->save_name          = realloc(saves->save_name, sizeof(char *) * saves->size);
+    saves->save_player_name   = realloc(saves->save_player_name, sizeof(char *) * saves->size);
+    saves->save_player_money  = realloc(saves->save_player_money, sizeof(double) * saves->size);
+    saves->time_spent_in_game = realloc(saves->time_spent_in_game, sizeof(unsigned int) * saves->size);
+    saves->game_seed          = realloc(saves->game_seed, sizeof(unsigned int) * saves->size);
+
+}
+
+int GetAllSavesCallback(void *saves, int argc, char **argv, char **col_name)
+{
+
+    if (argc == 0)
+        return -1;
+
+    PlayerSaves *temp = (PlayerSaves *)saves;
+
+    if (temp->num_saves == temp->size)
+        ResizePlayerSavesElements(temp);
+
+    temp->save_id[temp->num_saves]            = atoi(argv[0]);
+    temp->save_name[temp->num_saves]          = NULL;
+    temp->time_spent_in_game[temp->num_saves] = atoi(argv[2]);
+    temp->game_seed[temp->num_saves]          = atoi(argv[3]);
+    temp->save_player_id[temp->num_saves]     = atoi(argv[4]);
+    temp->save_player_name[temp->num_saves]   = NULL;
+    temp->save_player_money[temp->num_saves]  = atof(argv[6]);
+
+    return 0;
+
+}
+
+PlayerSaves *GetAllSaves() 
+{
+    PlayerSaves *saves = malloc(sizeof(PlayerSaves));
+
+    saves->num_saves          = 0;
+    saves->size               = 16;
+    saves->save_id            = malloc(sizeof(int) * 16);
+    saves->save_player_id     = malloc(sizeof(int) * 16);
+    saves->save_name          = malloc(sizeof(char *) * 16);
+    saves->save_player_name   = malloc(sizeof(char *) * 16);
+    saves->save_player_money  = malloc(sizeof(double) * 16);
+    saves->time_spent_in_game = malloc(sizeof(unsigned int) * 16);
+    saves->game_seed          = malloc(sizeof(unsigned int) * 16);
+
+    sqlite3 *db;
+    char *query = "SELECT S.SaveId, S.SaveName, S.TimeSpentInGame, S.RandomSeed, P.PlayerId, P.PlayerName, P.Money FROM Saves S "
+                  "INNER JOIN Players P ON P.SaveId = S.SaveId "
+                  "WHERE P.SaveOwner = 1";
+
+    if (OpenConnection(&db, DefaultConnection()) == 0)
+        ExecuteQuery(GetFormattedPointer(query), &GetAllSavesCallback, saves, db);
+
+    sqlite3_close(db);
+
+    return saves;
 
 }
