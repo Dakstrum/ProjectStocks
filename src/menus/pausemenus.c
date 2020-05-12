585d5676fb893742423ea95891a9616e9503d1d0
Emma Davenport <davenport.physics@gmail.com>
2020-05-11 21:51:39 -0500

    Removed unnecessary headers

--- src/menus/pausemenus.c
+++ src/menus/pausemenus.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-
-#include <allegro5/allegro.h>
 
 #include "jsonlayer.h"
 #include "drawlayers.h"
 #include "log.h"
-#include "shared.h"
-#include "stocksmenu.h"
-#include "newsmenu.h"
-#include "accountmenu.h"
-#include "cardsmenu.h"
-#include "mainmenu.h"
 #include "rendering.h"
 #include "account.h"
-#include "button.h"
-#include "scrollbox.h"
 #include "optionsmenu.h"
 #include "dbaccount.h"
 
