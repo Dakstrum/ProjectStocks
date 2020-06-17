9c2210aeb3f372aa5f447bcf19e454d1b9143e3c
Emma Davenport <davenport.physics@gmail.com>
2020-06-17 09:18:38 -0500

    Added products to seed data

--- sql/companies-seed.sql
+++ sql/companies-seed.sql
@@ -14,3 +14,16 @@ VALUES
     (35.0, 'Unimpressive Games', 1),
     (20.0, 'WhySpace', 2),
     (162.0, 'Interstellar Business Robots', 3);
+
+INSERT INTO
+	CompanyProducts (CompanyId, Name)
+VALUES
+	(1, 'Naon'),
+	(1, 'Backdoor Belief'),
+	(2, 'One Day Morning'),
+	(2, 'Screws of Peace'),
+	(2, 'Very Real Tournament'),
+	(3, 'Rat 1 Rocket'),
+	(3, 'Bird Space Shuttle'),
+	(4, 'Demonic A.I'),
+	(4, 'Space A.I (Whatever that means)');
