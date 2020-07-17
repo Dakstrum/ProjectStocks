1048a7a1e7896b97e66246332a136f6d59f8656f
Emma Davenport <davenport.physics@gmail.com>
2020-07-16 21:28:32 -0500

    Change: Removed debug log

--- src/main.c
+++ src/main.c
@@ -92,7 +92,6 @@ void Loop()
     } else {
 
         input_event = event;
-        LogF("event_type = %d, %d, %d", input_event.type, ALLEGRO_EVENT_KEY_DOWN, ALLEGRO_EVENT_KEY_UP);
         had_input   = true;
 
     }
