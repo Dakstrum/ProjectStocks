dae2455eaf6fbdf255c2b07ef6be7217924ac28f
Emma Davenport <davenport.physics@gmail.com>
2020-07-18 17:20:50 -0500

    Addition: Clang compiler toggle to build script

--- tools/build.sh
+++ tools/build.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
 
 declare build_dir=""
 declare operating_system=""
+declare c_compiler="gcc"
+declare cxx_compiler="g++"
+declare build_type="debug-build"
 
 if [ "$(uname -s)" == "Darwin" ]; then
 
@@ -14,6 +16,21 @@ else
 fi
 
 echo "Detected operating system = ${operating_system}"
+
+for arg in "$@"
+do
+    if [ $arg == "--release" ]; then
+
+        build_type="release-build"
+
+    elif [ $arg == "--clang" ]; then
+
+        c_compiler="clang"
+        cxx_compiler="clang++"
+
+    fi
+done
+
 DebugBuild() 
 {
 
@@ -23,12 +40,11 @@ DebugBuild()
 
     if [ $operating_system == "MINGW64_NT" ]; then
 
-        cmake .. -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_INSTALL_PREFIX="/mingw64/bin" -DCMAKE_BUILD_TYPE=Debug
+        cmake .. -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_INSTALL_PREFIX="/mingw64/bin" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=$c_compiler -DCMAKE_CXX_COMPILER=$cxx_compiler
 
     elif [ $operating_system == "Linux" ] || [ $operating_system == "Darwin" ]; then
 
-        echo "cmake"
-        cmake .. -DCMAKE_BUILD_TYPE=Debug
+        cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=$c_compiler -DCMAKE_CXX_COMPILER=$cxx_compiler
 
     fi
 
@@ -42,11 +58,11 @@ ReleaseBuild()
 
     if [ $operating_system == "MINGW64_NT" ]; then
 
-        cmake .. -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_INSTALL_PREFIX="/mingw64/bin" -DCMAKE_BUILD_TYPE=Release
+        cmake .. -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_INSTALL_PREFIX="/mingw64/bin" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_C_COMPILER=$c_compiler -DCMAKE_CXX_COMPILER=$cxx_compiler
 
     elif [ $operating_system == "Linux" ] || [ $operating_system == "Darwin" ]; then
 
-        cmake .. -DCMAKE_BUILD_TYPE=Release
+        cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=$c_compiler -DCMAKE_CXX_COMPILER=$cxx_compiler
 
     fi
 
@@ -59,7 +75,7 @@ GetSharedLibs()
 
         declare msys_path="C:/msys64/mingw64/bin/"
         for dll in "allegro_acodec-5.2.dll" "allegro_audio-5.2.dll" "allegro_font-5.2.dll" "allegro_image-5.2.dll" "allegro_ttf-5.2.dll" "allegro_video-5.2.dll" "allegro-5.2.dll" "allegro_primitives-5.2.dll" "libdumb.dll" \
-                   "libFLAC-8.dll" "libjson-c-4.dll" "libogg-0.dll" "libopenal-1.dll" "libopus-0.dll" "libopusfile-0.dll" "libsqlite3-0.dll" "libtheoradec-1.dll" "libvorbis-0.dll" "libvorbisfile-3.dll" \
+                   "libFLAC-8.dll" "libjson-c-5.dll" "libogg-0.dll" "libopenal-1.dll" "libopus-0.dll" "libopusfile-0.dll" "libsqlite3-0.dll" "libtheoradec-1.dll" "libvorbis-0.dll" "libvorbisfile-3.dll" \
                    "libpng16-16.dll" "libwebp-7.dll" "libgcc_s_seh-1.dll" "libstdc++-6.dll" "libjpeg-8.dll" "libfreetype-6.dll" "libwinpthread-1.dll" "zlib1.dll" "libbz2-1.dll" "libharfbuzz-0.dll" \
                    "libgraphite2.dll" "libglib-2.0-0.dll" "libpcre-1.dll" "libintl-8.dll" "libiconv-2.dll"; do
 
@@ -88,49 +104,60 @@ GetSharedLibs()
     fi
 
 }
-if [ $# -eq 0 ]; then
 
-	echo "Defaulting to --debug build"
-	DebugBuild
+SetupCmake()
+{
 
-elif [ $1 == "--debug" ]; then
+    if [ $build_type == "debug-build" ]; then
+        echo "Building debug version"
+        DebugBuild
+    else
+        echo "Building release version"
+        ReleaseBuild
+    fi
 
-    echo "Building debug version"
-    DebugBuild
-    
+}
 
-elif [ $1 == "--release" ]; then
+BuildProject()
+{
 
-    echo "Building release version"
-    ReleaseBuild
+    if [ $operating_system == "MINGW64_NT" ]; then
 
-fi
+        mingw32-make -j2
 
-if [ $operating_system == "MINGW64_NT" ]; then
+    elif [ $operating_system == "Linux" ] || [ $operating_system == "Darwin" ]; then
 
-    mingw32-make -j2
+        make -j2
 
-elif [ $operating_system == "Linux" ] || [ $operating_system == "Darwin" ]; then
+    fi
 
-    make -j2
+}
 
-fi
+main()
+{
 
-GetSharedLibs
+    SetupCmake
+    BuildProject
+    GetSharedLibs
 
-if [ $operating_system == "MINGW64_NT" ]; then
+    if [ $operating_system == "MINGW64_NT" ]; then
 
-    cp projectstocks.exe $build_dir
+        cp projectstocks.exe $build_dir
 
-elif [ $operating_system == "Linux" ]; then
+    elif [ $operating_system == "Linux" ]; then
 
-    cp projectstocks $build_dir
-    cp -r lib $build_dir
+        cp projectstocks $build_dir
+        cp -r lib $build_dir
 
-fi
+    fi
 
-if [ $operating_system == "MINGW64_NT" ] || [ $operating_system == "Linux" ]; then
+    if [ $operating_system == "MINGW64_NT" ] || [ $operating_system == "Linux" ]; then
 
-	cp -r assets $build_dir
+        cp -r assets $build_dir
 
-fi
+    fi
+
+}
+
+
+main
