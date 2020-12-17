5dca24f7c728efdd2c4e00351d1266834a4d6b4f
Emma Davenport <davenport.physics@gmail.com>
2020-12-17 09:19:20 -0600

    Addition: GetSubVectorRef

--- include/vector.h
+++ include/vector.h
@@ -24,6 +24,7 @@ void Vector_RemovePrt(Vector *vector, unsigned int element_idx);
 
 Vector *Vector_GetCopy(Vector *vector);
 Vector *Vector_GetSubVector(Vector *vector, size_t start_idx, size_t end_idx);
+Vector *Vector_GetSubVectorRef(Vector *vector, size_t start_idx, size_t end_idx);
 
 void Vector_Reset(Vector *vector);
 void Vector_Delete(Vector *vector);
