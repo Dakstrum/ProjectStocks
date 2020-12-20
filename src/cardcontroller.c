78326720614bad8e025232870ab3685845d076b2
Jack Soderstrom <JaxSoder@gmail.com>
2020-12-20 13:59:27 -0600

    Cleaned cardcontroller

--- src/cardcontroller.c
+++ src/cardcontroller.c
@@ -14,7 +14,7 @@ DrawObject *cardfive_button  = NULL;
 
 DrawObject *card_buttons[5];
 PlayerCard *player_cards;
-void *card_CBs[5];
+
 
 int number_of_cards = 0;
 int played_card = -1;
@@ -36,16 +36,10 @@ void CardController_InitCard()
 	card_buttons[3] = cardfour_button;
 	card_buttons[4] = cardfive_button;
 
-	card_CBs[0] = CardController_CardOne_CB;
-	card_CBs[1] = CardController_CardTwo_CB;
-	card_CBs[2] = CardController_CardThree_CB;
-	card_CBs[3] = CardController_CardFour_CB;
-	card_CBs[4] = CardController_CardFive_CB;
-
-	player_cards    = GetAllPlayerCards();
-	number_of_cards = GetNumOfPlayerCards();
+	void *card_CBs[5] = {CardController_CardOne_CB, CardController_CardTwo_CB, CardController_CardThree_CB, CardController_CardFour_CB, CardController_CardFive_CB};
+	player_cards      = GetAllPlayerCards();
+	number_of_cards   = GetNumOfPlayerCards();
 	
-
 	for(int i = 0; i < number_of_cards; i++)
 	{
 
@@ -60,19 +54,10 @@ void CardController_InitCard()
 		card_buttons[i]->asset_path = GetCardPath(player_cards[i].card_id);
 
 		AddObjectToDrawLayer(card_buttons[i]);
-		LogF("Created: x: %.2f, asset_path: %s", card_buttons[i]->x, card_buttons[i]->asset_path);
 	
 	}
-	LogF("CardId0: %d", player_cards[0].card_id);
-	LogF("CardId1: %d", player_cards[1].card_id);
-	LogF("CardId2: %d", player_cards[2].card_id);
-	LogF("CardId3: %d", player_cards[3].card_id);
-	LogF("CardId4: %d", player_cards[4].card_id);
-
-	LogF("CARD AMOOUNT = %d", GetNumOfPlayerCards());
 	
 }
-
 
 int GetCenteredPoint(int i)
 {
@@ -115,7 +100,6 @@ void CardController_AnimateCardUp(int card_num)
 {
 
 	Animate_MoveDrawObject(card_buttons[card_num], card_buttons[card_num]->x, 850, 500);
-	//Log("CARD UP");
 
 }
 
@@ -123,7 +107,6 @@ void CardController_AnimateCardDown(int card_num)
 {
 
 	Animate_MoveDrawObject(card_buttons[card_num], card_buttons[card_num]->x, 1009, 500);
-	//Log("CARD Down");
 
 }
 
@@ -136,55 +119,22 @@ void CardController_AnimateCardToCenterOfGraph(int card_num)
 
 void CardController_HoveringAnimationController(int card_num)
 {
+
 	if(card_buttons[card_num] == NULL)
 		return;
-	LogF("PlayedCard = %d", played_card);
+
 	if(played_card == card_num)
 	{
 		Animate_MoveDrawObject(card_buttons[card_num], 800, 300, 500);
-		
-		LogF("Moving card X: %.2f", card_buttons[card_num]->x);
-		if(card_buttons[card_num]->x == 800) //reaches middle
+
+		if(card_buttons[card_num]->x == 800)
 		{
 
-			LogF("CARD AMOOUNT IN LOOP = %d", GetNumOfPlayerCards());
-
-			if(card_buttons[0])
-			{
-				RemoveDrawObject(card_buttons[0]);
-				//card_buttons[0] = NULL;
-				Log("Removing 0");
-			}
-			if(card_buttons[1])
-			{
-				RemoveDrawObject(card_buttons[1]);
-				//card_buttons[1] = NULL;
-				Log("Removing 1");
-			}
-
-			if(card_buttons[2])
-			{
-				RemoveDrawObject(card_buttons[2]);
-				//card_buttons[2] = NULL;
-				Log("Removing 2");
-			}
-
-			if(card_buttons[3])
-			{
-				RemoveDrawObject(card_buttons[3]);
-				//card_buttons[3] = NULL;
-				Log("Removing 3");
-			}
-
-			if(card_buttons[4])
-			{
-				RemoveDrawObject(card_buttons[4]);
-				//card_buttons[4] = NULL;
-				Log("Removing 4");
-			}
+			for(int i = 0; i < 5; i++)
+				if(card_buttons[i])
+					RemoveDrawObject(card_buttons[i]);
 
 			CardController_InitCard();
-
 			played_card = -1;
 
 		}
@@ -206,9 +156,9 @@ void CardController_HoveringAnimationController(int card_num)
 
 void CardController_CardAnimationCheck()
 {
+	
 	for(int i = 0; i < 5; i++)
 		CardController_HoveringAnimationController(i);
-
 
 }
 
