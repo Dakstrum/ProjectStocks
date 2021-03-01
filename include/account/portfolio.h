bb2da75e0bef8f419c18fddca783999be851daa2
Emma Davenport <davenport.physics@gmail.com>
2021-03-01 12:11:24 -0600

    Addition: function portfolio_get_percentage

--- include/account/portfolio.h
+++ include/account/portfolio.h
@@ -4,5 +4,6 @@
 #include <stdint.h>
 
 float portfolio_get_networth(uint32_t player_id);
+float portfolio_get_percentage(uint32_t player_id, uint32_t company_id);
 
 #endif
--- src/account/portfolio.c
+++ src/account/portfolio.c
@@ -35,3 +35,25 @@ float portfolio_get_networth(uint32_t player_id)
 	return Account_GetMoney(player_id) + portfolio_get_asset_worth(player_id);
 
 }
+
+float portfolio_get_percentage(uint32_t player_id, uint32_t company_id)
+{
+
+	Vector *companies = GetAllCompaniesVector();
+	Company *companies_temp = companies->elements;
+
+	int specific_amount_owned  = 0;
+	int total_stocks_owned     = 0;
+	for (size_t i = 0;i < companies->num_elements;i++) {
+
+		int amount = dbaccount_get_owned_stock_amount(player_id, companies_temp[i].company_id);
+		if (company_id == companies_temp[i].company_id)
+			specific_amount_owned = amount;
+
+		total_stocks_owned += amount;
+
+	}
+
+	return (float)specific_amount_owned/(float)total_stocks_owned;
+
+}
