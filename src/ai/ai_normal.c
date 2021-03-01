eff78b06326489dfd9a3c7c8faaa0fddd9546013
Emma Davenport <davenport.physics@gmail.com>
2021-03-01 13:19:52 -0600

    Change: Attempt positive card playu

--- src/ai/ai_normal.c
+++ src/ai/ai_normal.c
@@ -3,45 +3,12 @@
 
 #include "dbsave.h"
 #include "dbcard.h"
+#include "shared.h"
 #include "dbcompany.h"
 #include "account.h"
 #include "portfolio.h"
 #include "transaction.h"
 #include "vector.h"
-
-/*
-
-typedef struct PlayerCard {
-
-    uint32_t player_card_id;
-    uint32_t player_id;
-    uint32_t card_id;
-
-} PlayerCard;
-
-void dbcard_apply_card(uint32_t player_id, uint32_t card_id, uint32_t company_id);
-
-typedef struct Company {
-
-    unsigned int company_id;
-    unsigned int category_id;
-    float ipo;
-    char company_name[64];
-    char company_description[256];
-    char company_abbreviation[6];
-
-} Company;
-
-typedef struct Player {
-
-    uint32_t player_id;
-    float money;
-    char name[32];
-    bool save_owner;
-
-} Player;
-
-*/
 
 bool ai_normal_does_other_own_at_least_percent(uint32_t player_id, uint32_t company_id, float percent)
 {
@@ -71,9 +38,7 @@ void ai_normal_attempt_negative_card_play(uint32_t player_id, uint32_t card_id)
 
     for (size_t i = 0; i < companies->num_elements;i++) {
 
-        float chance = 0.0f;
-        float owned_percentage = portfolio_get_percentage(player_id, companies_temp[i].company_id);
-        bool ai_owns_70_perc   = ai_normal_does_other_own_at_least_percent(player_id, companies_temp[i].company_id, 70.0f);
+
 
     }
 
@@ -87,7 +52,17 @@ void ai_normal_attempt_positive_card_play(uint32_t player_id, uint32_t card_id)
 
     for (size_t i = 0; i < companies->num_elements;i++) {
 
+        float owned_percentage = portfolio_get_percentage(player_id, companies_temp[i].company_id);
+        bool ai_owns_70_perc   = ai_normal_does_other_own_at_least_percent(player_id, companies_temp[i].company_id, 70.0f);
+        float random_chance    = shared_random_float();
+        float chance           = owned_percentage/30.0f - ai_owns_70_perc == true ? 1.0f : 0.0f;
 
+        if (chance > random_chance) {
+
+            dbcard_apply_card(player_id, card_id, companies_temp[i].company_id);
+            break;
+
+        }
 
     }
 
